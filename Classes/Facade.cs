9a9d42a6b6b128bddc18608c30164dd592b37e7a
Dmitry Alexandrov <adk230@yandex.ru>
2020-11-24 19:01:29 +0300

    created a facade

---
+++ Classes/Facade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc.Classes
+{
+	class Facade
+	{
+		public double Calculate(string expression)
+		{
+			throw new NotImplementedException();
+			//return 0;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+using Calc.Classes;
+
+/*
+Ideas for command line options:
+-h --help       show help
+-f --file       treat the argument as a file name, and get the expression from that file
+-c --no-color   don't color the output
+*/
+
 namespace Calc
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var facade = new Facade();
+			try
+			{
+				double result = facade.Calculate("test");
+				Console.Write(result);
+			}
+			catch (Exception e)
+			{
+				Console.Write($"Error: \"{e.Message}\"");
+			}
 		}
 	}
 }
