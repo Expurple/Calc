f4167fe0a15e00b8781ff9817075c403a6f6052b
Dmitry Alexandrov <adk230@yandex.ru>
2020-11-24 19:15:04 +0300

    created error codes and a command line argument

--- Program.cs
+++ Program.cs
@@ -13,18 +13,34 @@ namespace Calc
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			// This parsing is primitive, but I'll leave it for now
+			if (args.Length == 0)
+			{
+				Console.Write("Error: \"No math expression provided!\"");
+				return (int)ErrorCode.NoExpression;
+			}
+
 			var facade = new Facade();
 			try
 			{
-				double result = facade.Calculate("test");
+				double result = facade.Calculate(args[0]);
 				Console.Write(result);
+				return (int)ErrorCode.OK;
 			}
 			catch (Exception e)
 			{
 				Console.Write($"Error: \"{e.Message}\"");
+				return (int)ErrorCode.UnexpectedError;
 			}
 		}
+
+		enum ErrorCode
+		{
+			OK = 0,
+			UnexpectedError = -1,
+			NoExpression = -2,
+		};
 	}
 }
