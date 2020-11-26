b6d9efe9082e1ce454d58a0e595f6a9b4617ca2d
Dmitry Alexandrov <adk230@yandex.ru>
2020-11-26 20:19:54 +0300

    added more priority tests and found a brackets bug: "(A) + (B)" turns into "A) + (B" and then breaks

--- Tests/Functionality/Brackets.cs
+++ Tests/Functionality/Brackets.cs
@@ -14,10 +14,21 @@ namespace Calc.Tests.Functionality
 		}
 
 		[Test]
-		public void BracesPriority()
+		public void BracesPrioritySubtraction()
 		{
 			Assert.AreEqual(0, facade.Calculate("3 - 2 - 1"));
 			Assert.AreEqual(2, facade.Calculate("3 - (2 - 1)"));
+		}
+
+		[Test]
+		public void BracesPriorityMultiplication()
+		{
+			Assert.AreEqual(-2.25, facade.Calculate("1 * 2 + 3 / 4 - 5"));
+			Assert.AreEqual(-3.75, facade.Calculate("1 * (2 + 3) / 4 - 5"));
+			Assert.AreEqual(-1, facade.Calculate("1 * 2 + 3 / (4 - 5)"));
+			// Need to fix!
+			//Assert.AreEqual(-5, facade.Calculate("(1 * 2 + 3) / (4 - 5)"));
+			//Assert.AreEqual(-5, facade.Calculate("1 * ((2 + 3) / (4 - 5))"));
 		}
 	}
 }
